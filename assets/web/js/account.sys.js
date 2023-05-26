4dd937805a04b96282fa1cb54d6b8d71c6ac291d
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2023-05-26 17:33:26 +0800

    Add files via upload

---
+++ assets/web/js/account.sys.js
@@ -0,0 +1,12 @@
+function Login() {
+    let username = document.getElementById('user')
+    let password = document.getElementById('pass')
+
+    if (username.value == 'user' && password.value == 'admin') {
+        console.log('logged in')
+        window.location.href = "/dir/main.html"
+        sessionStorage.setItem('onlinepalengke_user',user)
+    } else {
+        alert('Wrong username or password')
+    }
+}
